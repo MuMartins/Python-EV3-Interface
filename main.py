2a832b6f3c4e5b1b79baca3c89740ad2fee514d4
Murillo Martins <murillomartins@uol.com.br>
2021-08-30 21:59:57 -0300

    main-update

--- main.py
+++ main.py
@@ -14,98 +14,68 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # Create your objects here.
 ev3 = EV3Brick()
-battery = ev3.battery.voltage()
+
+# Functions
+def button_pressed_def(ev3):
+    pressed = []
+    while len(pressed) != 1:
+        pressed = ev3.buttons.pressed()
+    button = pressed[0]
+
+    # Now wait for the button to be released.
+    while any(ev3.buttons.pressed()):
+        pass
+
+    # Return which button was pressed.
+    return button
 
 # Write your program here.
-screen.clear() #reseta a tela do ev3
 menu_interface_selecionador = 1
 
-while True: #menu-seleção
-    # MENU DISPLAY
-    if menu_interface_selecionador == 1:
-        screen.load_image(display_menu.png)
+def main():
+    while True: #menu-seleção
+        button_pressed = button_pressed_def(ev3)
 
-    elif menu_interface_selecionador == 2:
-        screen.load_image(display_calibração.png)
-    
-    elif menu_interface_selecionador == 3:
-        screen.load_image(display_problemas.png)
+        # MENU DISPLAY
+        if menu_interface_selecionador == 1:
+            ev3.screen.load_image('display_menu.png')
 
-    elif menu_interface_selecionador == 4:
-        screen.load_image(display_portview.png)
+        elif menu_interface_selecionador == 2:
+            ev3.screen.load_image('display_calibração.png')
+        
+        elif menu_interface_selecionador == 3:
+            ev3.screen.load_image('display_problemas.png')
 
-    
-    # MENU SELEÇÃO
-    if button.DOWN in ev3.buttons.pressed():
-        menu_interface_selecionador += 2
-        if menu_interface_selecionador == 5:
-            menu_interface_selecionador = 1
-        if menu_interface_selecionador == 6:
-            menu_interface_selecionador = 2
+        elif menu_interface_selecionador == 4:
+            ev3.screen.load_image('display_portview.png')
 
-    elif button.UP in ev3.buttons.pressed():
-        menu_interface_selecionador -= 2
-        if menu_interface_selecionador == 0:
-            menu_interface_selecionador = 4
-        if menu_interface_selecionador == -1:
-            menu_interface_selecionador = 3
+        
+        # MENU SELEÇÃO
+        if button_pressed == Button.DOWN:
+            menu_interface_selecionador += 2
+            if menu_interface_selecionador == 5:
+                menu_interface_selecionador = 1
+            if menu_interface_selecionador == 6:
+                menu_interface_selecionador = 2
 
-    elif button.RIGHT in ev3.buttons.pressed():
-        menu_interface_selecionador += 1
-        if menu_interface_selecionador == 5:
-            menu_interface_selecionador = 1
-    
-    elif button.LEFT in ev3.buttons.pressed():
-        menu_interface_selecionador -= 1
-        if menu_interface_selecionador == 0:
-            menu_interface_selecionador = 4
+        elif button_pressed == Button.UP:
+            menu_interface_selecionador -= 2
+            if menu_interface_selecionador == 0:
+                menu_interface_selecionador = 4
+            if menu_interface_selecionador < 0:
+                menu_interface_selecionador = 3
 
+        elif button_pressed == Button.RIGHT:
+            menu_interface_selecionador += 1
+            if menu_interface_selecionador == 5:
+                menu_interface_selecionador = 1
+        
+        elif button_pressed == Button.LEFT:
+            menu_interface_selecionador -= 1
+            if menu_interface_selecionador == 0:
+                menu_interface_selecionador = 4
 
-
-
-# while True: #menu-main
-#     screen.clear() #reseta a tela do ev3
-#     menu_interface_selecionador = 1
-#     while True: #menu-funções-01
-#         while True: #menu-seleção
-#             # MENU DISPLAY
-#             if menu_interface_selecionador == 1:
-#                 screen.load_image(display_menu.png)
-
-#             elif menu_interface_selecionador == 2:
-#                 screen.load_image(display_calibração.png)
-            
-#             elif menu_interface_selecionador == 3:
-#                 screen.load_image(display_problemas.png)
-
-#             elif menu_interface_selecionador == 4:
-#                 screen.load_image(display_portview.png)
-
-            
-#             # MENU SELEÇÃO
-#             if button.DOWN in ev3.buttons.pressed():
-#                 menu_interface_selecionador += 2
-#                 if menu_interface_selecionador == 5:
-#                     menu_interface_selecionador = 1
-#                 if menu_interface_selecionador == 6:
-#                     menu_interface_selecionador = 2
-
-#             elif button.UP in ev3.buttons.pressed():
-#                 menu_interface_selecionador -= 2
-#                 if menu_interface_selecionador == 0:
-#                     menu_interface_selecionador = 4
-#                 if menu_interface_selecionador == -1:
-#                     menu_interface_selecionador = 3
-
-#             elif button.RIGHT in ev3.buttons.pressed():
-#                 menu_interface_selecionador += 1
-#                 if menu_interface_selecionador == 5:
-#                     menu_interface_selecionador = 1
-            
-#             elif button.LEFT in ev3.buttons.pressed():
-#                 menu_interface_selecionador -= 1
-#                 if menu_interface_selecionador == 0:
-#                     menu_interface_selecionador = 4
-#         while True: #menu-funções
-
-#     while True: #menu-funções-02
+# Run the code
+if __name__ == '__main__':
+    # Executa nossa função principal
+    main()
