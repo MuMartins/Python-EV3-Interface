01bf18e1400f044109ac86765f7c2aef8d64c88c
Murillo Martins <murillomartins@uol.com.br>
2021-08-31 23:11:09 -0300

    rounds-update

--- main.py
+++ main.py
@@ -1,56 +1,141 @@
 #!/usr/bin/env pybricks-micropython
+# Importação dos módulos utilizados
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.parameters import Button, Color
+from pybricks.tools import wait
+
 from main_def import *
-# Create your objects here.
+
+# Definição do brick como ev3
 ev3 = EV3Brick()
-# Write your program here.
-while True: #menu-seleção
-    menu_interface_selecionador = 0
-    interface_selecionador = 0
-    while True:
-        # MENU DISPLAY
-        if menu_interface_selecionador == 0:
-            ev3.screen.load_image('./IMG_MENU/display_menu.png')
 
-        if menu_interface_selecionador == 1:
-            ev3.screen.load_image('./IMG_MENU/display_calibração.png')
-        
-        if menu_interface_selecionador == 2:
-            ev3.screen.load_image('./IMG_MENU/display_problemas.png')
+# Código principal
+menu_selecionador = [
+    # A ordem é X1, Y1, X2, Y2, Preenchimento, Cor
+    [3, 14, 85, 62, 1, False, Color.BLACK],     # MENU
+    [90, 14, 172, 62, 1, False, Color.BLACK],   # CALIBRACAO
+    [3, 65, 85, 113, 1, False, Color.BLACK],    # PROBLEMAS
+    [90, 65, 172, 113, 1, False, Color.BLACK]   # PORT_VIEW
+]
 
-        if menu_interface_selecionador == 3:
-            ev3.screen.load_image('./IMG_MENU/display_portview.png')
+# Variáveis
+menu_selecionador_single = 0
+menu_selecionador_single_last = 3
 
-        # MENU SELEÇÃO
-        if button_pressed(Button.DOWN):
-            while not button_released(Button.DOWN):
-                wait(10)
-            menu_interface_selecionador = (menu_interface_selecionador + 2) % 4
+round_selecionador = 0
 
-        if button_pressed(Button.UP):
-            while not button_released(Button.UP):
-                wait(10)
-            menu_interface_selecionador = (menu_interface_selecionador - 2) % 4
+# Função menu
+def set_menu_selecionador():
+    global menu_selecionador, menu_selecionador_single_last
+    if menu_selecionador_single != menu_selecionador_single_last:
+        ev3.screen.load_image('./IMG_MENU/display.png')
+        ev3.screen.draw_box(*menu_selecionador[menu_selecionador_single])
+        menu_selecionador_single_last = menu_selecionador_single
 
-        if button_pressed(Button.RIGHT):
-            while not button_released(Button.RIGHT):
-                wait(10)
-            menu_interface_selecionador = (menu_interface_selecionador + 1) % 4
+# Code
+while True:
+    set_menu_selecionador()
 
-        if button_pressed(Button.LEFT):
-            while not button_released(Button.LEFT):
-                wait(10)
-            menu_interface_selecionador = (menu_interface_selecionador - 1) % 4
-        
-        # MENU FUNÇÃO
-        if menu_interface_selecionador == 0 and Button.CENTER in ev3.buttons.pressed():
+    # Interface selecionador
+    if button_pressed(Button.DOWN):
+        while not button_released(Button.DOWN):
+            wait(10)
+        menu_selecionador_single = (menu_selecionador_single + 2) % 4
+    
+    elif button_pressed(Button.UP):
+        while not button_released(Button.UP):
+            wait(10)
+        menu_selecionador_single = (menu_selecionador_single - 2) % 4
+    
+    elif button_pressed(Button.RIGHT):
+        while not button_released(Button.RIGHT):
+            wait(10)
+        menu_selecionador_single = (menu_selecionador_single + 1) % 4
+    
+    elif button_pressed(Button.LEFT):
+        while not button_released(Button.LEFT):
+            wait(10)
+        menu_selecionador_single = (menu_selecionador_single - 1) % 4
+    
+    # Interface função
+    elif button_pressed(Button.CENTER):
+        if menu_selecionador_single == 0:
+            while True:
+                if button_pressed(Button.RIGHT):
+                    while not button_released(Button.RIGHT):
+                        wait(10)
+                    round_selecionador = (round_selecionador + 1) % 4
+                
+                elif button_pressed(Button.LEFT):
+                    while not button_released(Button.LEFT):
+                        wait(10)
+                    round_selecionador = (round_selecionador - 1) % 4
+                
+                if round_selecionador == 0:
+                    ev3.screen.load_image('./IMG_ROUNDS/round_01')
+                
+                if round_selecionador == 1:
+                    ev3.screen.load_image('./IMG_ROUNDS/round_02')
+
+                if round_selecionador == 2:
+                    ev3.screen.load_image('./IMG_ROUNDS/round_03')
+
+                if round_selecionador == 3:
+                    ev3.screen.load_image('./IMG_ROUNDS/round_04')
+
+                if button_pressed(Button.CENTER):
+                    if round_selecionador == 0:
+                        ev3.screen.clear()
+                        while True:
+                            if button_pressed(Button.DOWN):
+                                break
+                            else:
+                                ev3.screen.draw_circle(70, 90, 20, fill=True)
+
+                    if round_selecionador == 1:
+                        ev3.screen.clear()
+                        while True:
+                            if button_pressed(Button.DOWN):
+                                break
+                            else:
+                                ev3.screen.draw_line(30,30,30,100)
+
+                    if round_selecionador == 2:
+                        ev3.screen.clear()
+                        while True:
+                            if button_pressed(Button.DOWN):
+                                break
+                            else:
+                                ev3.screen.draw_line(30,30,30,100)
+                    
+                    if round_selecionador == 3:
+                        ev3.screen.clear()
+                        while True:
+                            if button_pressed(Button.DOWN):
+                                break
+                            else:
+                                ev3.screen.draw_box(50,30,90,60)
+
+        elif menu_selecionador_single == 1:
+            ev3.screen.clear()
             while True:
                 if button_pressed(Button.DOWN):
                     break
-                else: 
-                    menu_rounds()
+                else:
+                    ev3.screen.draw_circle(70, 90, 20, fill=True)
+        
+        elif menu_selecionador_single == 2:
+            ev3.screen.clear()
+            while True:
+                if button_pressed(Button.DOWN):
+                    break
+                else:
+                    ev3.screen.draw_line(30,30,30,100)
+        
+        elif menu_selecionador_single == 3:
+            ev3.screen.clear()
+            while True:
+                if button_pressed(Button.DOWN):
+                    break
+                else:
+                    ev3.screen.draw_box(50,30,90,60)
--- main_def.py
+++ main_def.py
@@ -17,9 +17,3 @@ def wait_button_bumped(button):
     
     while not button_released(button):
         wait(10)    
-
-
-def menu_rounds():
-    ev3.screen.load_image('./IMG_ROUNDS/round_01.png')
-    if button_pressed(Button.CENTER):
-        ev3.speaker.beep()
