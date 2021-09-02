ea574869a948850e16744205fd31570d4eb56fbc
Administrador@EG-DC.COM <Administrador@EG-DC.COM>
2021-09-02 16:56:57 -0300

    thread-update

--- interface_rounds.py
+++ interface_rounds.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env pybricks-micropython
 # Importação dos módulos utilizados
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
+from pybricks.ev3devices import Motor, Stop
 from pybricks.parameters import Button, Color, Port
 from pybricks.tools import wait
 from pybricks.robotics import DriveBase
 
 from system_buttons import *
+from round_01 import *
+from round_02 import *
+from round_03 import *
+from round_04 import *
+
+from threading import Thread
 
 # Definição do brick como ev3
 ev3 = EV3Brick()
 
-# # Iniciando os motores
-# right_motor = Motor(Port.A)
-# left_motor = Motor(Port.D)
-
-# # Drive base (aqui será adicionado tudo referente ao robô, fazendo o calculo automático de giro, milimitros e etc)
-# luis = DriveBase(left_motor, right_motor, wheel_diameter=49.5, axle_track=9.5)
+# Variável responsável por acabar o round e começar o proximo
+saida_fim = False
 
 # Código
-def set_menu():
+def set_rounds():
     round_selecionador = 0
+    global saida_fim
     while True:
+        # Selecionador
         if button_pressed(Button.RIGHT):
             while not button_released(Button.RIGHT):
                 wait(10)
@@ -32,51 +36,47 @@ def set_menu():
                 wait(10)
             round_selecionador = (round_selecionador - 1) % 4
         
+        # Display e função
         if round_selecionador == 0:
             ev3.screen.load_image('./IMG_ROUNDS/round_01')
+            if button_pressed(Button.CENTER):
+                Thread(target=round_01(saida_fim)).start()
+
+                while not saida_fim:
+                    if button_pressed(Button.DOWN):
+                        robot.stop()
+                        round_selecionador = (round_selecionador + 1) % 4
+                        saida_fim = True
         
         if round_selecionador == 1:
             ev3.screen.load_image('./IMG_ROUNDS/round_02')
+            if button_pressed(Button.CENTER):
+                Thread(target=round_02(saida_fim)).start()
+
+                while not saida_fim:
+                    if button_pressed(Button.DOWN):
+                        robot.stop()
+                        round_selecionador = (round_selecionador + 1) % 4
+                        saida_fim = True
 
         if round_selecionador == 2:
             ev3.screen.load_image('./IMG_ROUNDS/round_03')
+            if button_pressed(Button.CENTER):
+                Thread(target=round_03(saida_fim)).start()
+
+                while not saida_fim:
+                    if button_pressed(Button.DOWN):
+                        robot.stop()
+                        round_selecionador = (round_selecionador + 1) % 4
+                        saida_fim = True
 
         if round_selecionador == 3:
             ev3.screen.load_image('./IMG_ROUNDS/round_04')
+            if button_pressed(Button.CENTER):
+                Thread(target=round_04(saida_fim)).start()
 
-        # if button_pressed(Button.CENTER):
-        #     if round_selecionador == 0:
-        #         ev3.screen.clear()
-        #         while True:
-        #             if button_pressed(Button.DOWN):
-        #                 break
-        #             else:
-        #                 luis.straight(1000)
-        #                 ev3.speaker.beep()
-
-        #     if round_selecionador == 1:
-        #         ev3.screen.clear()
-        #         while True:
-        #             if button_pressed(Button.DOWN):
-        #                 break
-        #             else:
-        #                 luis.straight(-1000)
-        #                 ev3.speaker.beep()
-
-        #     if round_selecionador == 2:
-        #         ev3.screen.clear()
-        #         while True:
-        #             if button_pressed(Button.DOWN):
-        #                 break
-        #             else:
-        #                 luis.turn(360)
-        #                 ev3.speaker.beep()
-            
-        #     if round_selecionador == 3:
-        #         ev3.screen.clear()
-        #         while True:
-        #             if button_pressed(Button.DOWN):
-        #                 break
-        #             else:
-        #                 luis.turn(-360)
-        #                 ev3.speaker.beep()
+                while not saida_fim:
+                    if button_pressed(Button.DOWN):
+                        robot.stop()
+                        round_selecionador = (round_selecionador + 1) % 4
+                        saida_fim = True
---
+++ round_03.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env pybricks-micropython
+# Importação dos módulos utilizados
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Button, Color, Port
+from pybricks.tools import wait
+from pybricks.robotics import DriveBase
+
+from system_buttons import *
+from cm_e_curva import *
+
+# Definição do brick como ev3
+ev3 = EV3Brick()
+
+# Iniciando os motores
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+# Drive base (aqui será adicionado tudo referente ao robô, fazendo o calculo automático de giro, milimitros e etc)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=49.5, axle_track=95)
+
+# Código 
+def round_03(saida_fim):
+    robot.turn(360)
+    ev3.speaker.beep()
+    saida_fim = True
+    return saida_fim
