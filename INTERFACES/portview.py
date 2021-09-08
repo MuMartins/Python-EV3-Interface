84c014bce1fd7217b2536254ed321a41e6e92340
Murillo Martins <murillomartins@uol.com.br>
2021-09-08 14:07:03 -0300

    portview-v.02

--- INTERFACES/portview.py
+++ INTERFACES/portview.py
@@ -2,6 +2,7 @@
 # Importação dos módulos utilizados
 from pybricks.hubs import EV3Brick
 from pybricks.parameters import Button, Color, Port
+from pybricks.ev3devices import Motor, ColorSensor, GyroSensor
 from pybricks.media.ev3dev import Font
 
 import SYSTEM.battery as battery
@@ -21,6 +22,15 @@ ports_coordinates = [
     [66, 91]   # Port.D
 ]
 
+light_sensor_01 = ColorSensor(Port.S1)
+light_sensor_02 = ColorSensor(Port.S2)
+light_sensor_03 = ColorSensor(Port.S3)
+
+medium_A_motor = Motor(Port.A)
+right_motor = Motor(Port.B)
+left_motor = Motor(Port.C)
+medium_D_motor = Motor(Port.D)
+
 
 def start():
     ev3.screen.clear()
@@ -31,20 +41,20 @@ def start():
         battery.voltage_text()
         # Sensores
         ev3.screen.draw_text(
-            *ports_coordinates[0], text=int(check_portview.sensor(Port.S1)))
+            *ports_coordinates[0], text=int(light_sensor_01.reflection()))
         ev3.screen.draw_text(
-            *ports_coordinates[1], text=int(check_portview.sensor(Port.S2)))
+            *ports_coordinates[1], text=int(light_sensor_02.reflection()))
         ev3.screen.draw_text(
-            *ports_coordinates[2], text=int(check_portview.sensor(Port.S3)))
+            *ports_coordinates[2], text=int(light_sensor_03.reflection()))
 
         # Motores
         ev3.screen.draw_text(
-            *ports_coordinates[3], text=int(check_portview.motor(Port.A)))
+            *ports_coordinates[3], text=int(medium_A_motor.angle()))
         ev3.screen.draw_text(
-            *ports_coordinates[4], text=int(check_portview.motor(Port.B)))
+            *ports_coordinates[4], text=int(right_motor.angle()))
         ev3.screen.draw_text(
-            *ports_coordinates[5], text=int(check_portview.motor(Port.C)))
+            *ports_coordinates[5], text=int(left_motor.angle()))
         ev3.screen.draw_text(
-            *ports_coordinates[6], text=int(check_portview.motor(Port.D)))
+            *ports_coordinates[6], text=int(medium_D_motor.angle()))
 
     ev3.screen.clear()
